ba91ecc8bd4f0f0487fab2675d88e81d783fbb16
Anatolii Sakhnik <sakhnik@gmail.com>
2018-01-14 18:47:45 +0200

    Configure /etc/shells

--- 25-base.sh
+++ 25-base.sh
@@ -1,7 +1,11 @@
+AddPackage git # the fast distributed version control system
 AddPackage glibc # GNU C Library
 AddPackage f2fs-tools # Tools for Flash-Friendly File System (F2FS)
 AddPackage nano # Pico editor clone with enhancements
 AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage zsh # A very advanced and programmable command interpreter (shell) for UNIX
+AddPackage zsh-completions # Additional completion definitions for Zsh
+AddPackage zsh-lovers # A collection of tips, tricks and examples for the Z shell.
 
 # Specify locales
 f="$(GetPackageOriginalFile glibc /etc/locale.gen)"
@@ -18,3 +22,9 @@ FONT=LatArCyrHeb-19
 EOF
 
 sed -i 's/^#\( %wheel ALL=(ALL) ALL\)$/\1/' "$(GetPackageOriginalFile sudo /etc/sudoers)"
+
+cat >>"$(GetPackageOriginalFile filesystem /etc/shells)" <<EOF
+/bin/zsh
+/usr/bin/zsh
+/usr/bin/git-shell
+EOF
