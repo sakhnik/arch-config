f420c0528579759242225a552151286f8ac5e384
Anatolii Sakhnik <sakhnik@gmail.com>
2018-01-14 18:03:09 +0200

    Record pacman.conf

--- 20-pacman.sh
+++ 20-pacman.sh
@@ -1,3 +1,6 @@
+AddPackage pacman # A library-based package manager with dependency support
+AddPackage pacutils # Helper tools for libalpm
+
 cat >"$(CreateFile /etc/pacman.d/hooks/paccache-remove.hook)" <<EOF
 [Trigger]
 Operation = Remove
@@ -20,4 +23,18 @@ Target = *
 Description = Cleaning pacman cache...
 When = PostTransaction
 Exec = /usr/bin/paccache -rk2
+EOF
+
+sed -i '/^## Ukraine/,/^$/ s/^#//' "$(GetPackageOriginalFile pacman-mirrorlist /etc/pacman.d/mirrorlist)"
+
+sed -i -f - "$(GetPackageOriginalFile pacman /etc/pacman.conf)" <<EOF
+/^#Color/ s/^#//
+/^#TotalDownload/ s/^#//
+/^#CheckSpace/ s/^#//
+/^#VerbosePkgLists/ s/^#//
+/VerbosePkgLists/ a ILoveCandy
+/#\[testing\]/,/^$/ s/^#//
+/#\[community-testing\]/,/^$/ s/^#//
+/#\[multilib-testing\]/,/^$/ s/^#//
+/#\[multilib\]/,/^$/ s/^#//
 EOF
