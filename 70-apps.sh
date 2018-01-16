1d91e32c81448fa9d79653158816c272a0526c11
Anatolii Sakhnik <sakhnik@gmail.com>
2018-01-16 08:17:03 +0200

    Store configuration of mc

--- 70-apps.sh
+++ 70-apps.sh
@@ -1,3 +1,13 @@
+AddPackage make # GNU make utility to maintain groups of programs
+AddPackage man-db # A utility for reading man pages
+AddPackage man-pages # Linux man pages
+AddPackage mc # Midnight Commander is a text based filemanager/shell that emulates Norton Commander
+AddPackage mercurial # A scalable distributed SCM tool
+AddPackage mpv # a free, open source, and cross-platform media player
+
+CreateLink /etc/mc/mc.keymap mc.default.keymap
+SetFileProperty /etc/mc/mc.keymap mode 777
+
 cat >"$(CreateFile /etc/mpv/mpv.conf)" <<EOF
 hwdec=vaapi
 #vo=opengl
