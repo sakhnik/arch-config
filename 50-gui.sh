a8aba165a5b128ea66fb0fd6568392806350861c
Anatolii Sakhnik <sakhnik@gmail.com>
2018-03-20 09:52:59 +0200

    Add sway

--- 50-gui.sh
+++ 50-gui.sh
@@ -8,6 +8,7 @@ AddPackage i3blocks # Define blocks for your i3bar status line
 AddPackage i3lock # An improved screenlocker based upon XCB and PAM
 AddPackage i3status # Generates status bar to use with i3bar, dzen2 or xmobar
 AddPackage lightdm-gtk-greeter-settings # Settings editor for the LightDM GTK+ Greeter
+AddPackage sway # i3 compatible window manager for Wayland
 AddPackage xautolock # An automatic X screen-locker/screen-saver
 AddPackage xclip # Command line interface to the X11 clipboard
 AddPackage xcompmgr # Composite Window-effects manager for X.org
