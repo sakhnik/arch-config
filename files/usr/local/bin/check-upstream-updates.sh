3aa47f8b7bfaaca826773b792b129605789e3c08
Anatolii Sakhnik <sakhnik@gmail.com>
2019-02-24 07:28:49 +0200

    Ignore /srv/http/

--- files/usr/local/bin/check-upstream-updates.sh
+++ files/usr/local/bin/check-upstream-updates.sh
@@ -3,8 +3,6 @@
 
 export LANG=en_US.UTF-8
 
-auracle sync
-
 kernel_actual=`pacman -Qi linux-lts49 2>/dev/null | perl -ne 'print "$1" if /Version\s+:\s+4\.9\.(\d+)/'`
 if [[ ! ${PIPESTATUS[0]} ]]; then
 	kernel_available=`curl https://www.kernel.org 2>/dev/null | perl -ne 'print "$1" if /linux-4\.9\.(\d+).tar.xz/'`
