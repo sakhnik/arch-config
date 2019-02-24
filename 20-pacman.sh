e87b5ce0d3e1410013d74c815d2a97f4a1bd10d5
Anatolii Sakhnik <sakhnik@gmail.com>
2019-02-24 10:00:15 +0200

    Update mirrorlist

--- 20-pacman.sh
+++ 20-pacman.sh
@@ -56,10 +56,12 @@ EOF
 
 CopyFile /usr/local/bin/check-upstream-updates.sh 755
 
-sed -i -f - "$(GetPackageOriginalFile pacman-mirrorlist /etc/pacman.d/mirrorlist)" <<EOF
+sed -i -f - "$(GetPackageOriginalFile pacman-mirrorlist /etc/pacman.d/mirrorlist)" <<'EOF'
 4 a
-4 a Server = http://mirrors.nix.org.ua/linux/archlinux/\$repo/os/\$arch
-4 a Server = https://mirrors.nix.org.ua/linux/archlinux/\$repo/os/\$arch
+4 a Server = http://archlinux.ip-connect.vn.ua/$repo/os/$arch
+4 a Server = https://archlinux.ip-connect.vn.ua/$repo/os/$arch
+4 a Server = http://mirrors.nix.org.ua/linux/archlinux/$repo/os/$arch
+4 a Server = https://mirrors.nix.org.ua/linux/archlinux/$repo/os/$arch
 EOF
 
 sed -i -f - "$(GetPackageOriginalFile pacman /etc/pacman.conf)" <<EOF
